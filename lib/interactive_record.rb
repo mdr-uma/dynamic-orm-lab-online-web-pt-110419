2ef7e4949f2be65290d93ff1fd88558707fe8b5c
Uma Manandhar <um_2003_8@hotmail.com>
2020-02-04 21:55:36 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -47,4 +47,10 @@ class InteractiveRecord
     end
     values.join(", ")
   end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+    DB[:conn].execute(sql, name)
+  end
+
 end
