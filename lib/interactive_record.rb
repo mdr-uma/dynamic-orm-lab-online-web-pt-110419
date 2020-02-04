4612fc6f83e7c1b7777ed8499e05e3f65ce94b13
Uma Manandhar <um_2003_8@hotmail.com>
2020-02-04 22:01:54 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,7 +53,7 @@ class InteractiveRecord
     DB[:conn].execute(sql, name)
   end
 
-  def self.find_by(column_names)
+  def self.find_by(id, name, grade)
     sql = "SELECT * FROM #{self.table_name} WHERE column_names = ?"
     DB[:conn].execute(sql, column_names)
   end
